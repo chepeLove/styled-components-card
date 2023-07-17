45c1c563bb0eaf9d9c6862fe738a734f914f62ec
chepeLove <ilya.chepelov@gmail.com>
2023-07-17 18:46:13 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+import CardImageSrc from './assets/images/Rectangle.svg'
+import {Card} from "./components/Card.styled";
+import {CardBody} from "./components/CardBody.styled";
+import {CardTitle} from "./components/CardTitle.styled";
+import {CardButton, CardButtonBox} from "./components/CardButton.styled";
+import { CardText } from './components/CardText.styled';
+import { CardImage } from './components/CardImage.styled';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Box>
+                <Card>
+                    <CardImage src = {CardImageSrc}/>
+                    <CardBody>
+                        <CardTitle>Headline</CardTitle>
+                        <CardText>Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.</CardText>
+                        <CardButtonBox>
+                            <CardButton >See more</CardButton>
+                            <CardButton  >Save</CardButton>
+                        </CardButtonBox>
+                    </CardBody>
+                </Card>
+            </Box>
+        </div>
+    );
 }
 
 export default App;
+
+const Box = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
